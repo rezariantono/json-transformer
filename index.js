22b102ad104d060e6b6210894647129b143431d7
Reza Sukarno <rezariantono@gmail.com>
2018-07-10 10:04:52 +0700

    Belum bener karena masih nge chache requestIncludes  dan requestExcludes

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 function serializer(mapping) {
 
     this.mapping = mapping
+    this.requestIncludes = []
+    this.requestExcludes = []
+
+    this.middleware = (req, res, next) => {
+        
+        this.requestIncludes.push((req.query.includes || req.query.include || req.query.with || '').split(','))
+        this.requestExcludes.push((req.query.excludes || req.query.exclude || req.query.without || '').split(','))
+        next()
+    }
 
     this.setMapping = (mapping) => {
         this.mapping = mapping
+    }
+
+    this.getIncludes = () => {
+
+        var includes = []
+
+        if (this.mapping.defaultIncludes) {
+            includes = includes.concat(this.mapping.defaultIncludes.split(','))
+        }
+
+        if (this.mapping.availableIncludes) {
+
+            var filteredAvailableIncludes = this.mapping.availableIncludes.split(',').filter((availableIncludeString) => {
+                return this.requestIncludes.includes(availableIncludeString)
+            })
+            includes = includes.concat(filteredAvailableIncludes)
+        }
+
+        console.log(includes)
+
+        if (this.requestExcludes.length >= 1) {
+            includes = includes.filter((include) => {
+                return !this.requestExcludes.includes(include)
+            })
+        }
+
+        console.log(includes)
+        console.log(this.requestExcludes)
+
+        return includes
+
     }
 
     this.item = (item) => {
 
         const data = this.mapping.transform(item)
 
-        if (this.mapping.defaultIncludes) {
-            const includes = this.mapping.defaultIncludes.split(',')
+        const includes = this.getIncludes()
 
-            includes.forEach((include) => {
-                data[include] = this.mapping['include' + include.replace(/^\w/, c => c.toUpperCase())](item)
-            })
-        }
+        includes.forEach((include) => {
+
+            const includeFunctionName = 'include' + include.replace(/^\w/, c => c.toUpperCase())
+
+            if (typeof this.mapping[includeFunctionName] != 'function') {
+                throw new Error(includeFunctionName + ' is not defined in transformer')
+            }
+
+            data[include] = this.mapping[includeFunctionName](item)
+        })
 
         return data
     }
-
 
     this.transformItem = (item, meta) => {
 
